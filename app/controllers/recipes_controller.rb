127572d86eb20284b35c396fca4ce72850d7ab43
Jethro Williams <jethrowilliams4@hotmail.com>
2019-12-31 14:59:56 +0000

    Update application to haml

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
+  def index
+    @recipes = Recipe.all # UPDATE THIS
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     authorize @recipe
-    puts "⛱⛱⛱⛱⛱⛱⛱⛱⛱⛱⛱⛱"
-    puts current_user if current_user
-    puts "⛱⛱⛱⛱⛱⛱⛱⛱⛱⛱⛱⛱"
   end
 end
