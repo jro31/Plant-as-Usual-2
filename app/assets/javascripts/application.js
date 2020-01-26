c873fcadf5a97350ba2d957bbfdcd2f0f877566b
Jethro Williams <jethrowilliams4@hotmail.com>
2020-01-26 15:31:09 +0000

    Move dark_mode js to separate file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,34 +1,3 @@
 //= require rails-ujs
-//= require_tree .
 //= require jquery3
-
-var darkModeClasses = ['navbar', 'page-container', 'recipe-card']
-
-$(function() {
-  $('#dark-mode').click(function() {
-      toggleClasses();
-      selectMode();
-  });
-});
-
-function toggleClasses() {
-  $(darkModeClasses).each(function() {
-    $(`.${this}`).toggleClass(`${this}-light-mode ${this}-dark-mode`)
-  });
-}
-
-
-function selectMode() {
-  $.get('/current_user_data', function(result) {
-    var url = `/users/toggle_dark_mode/${result.id}`
-    var newDarkMode = !result.dark_mode
-    $.ajax({
-      type: 'patch',
-      url: url,
-      dataType: 'json',
-      data: { 'user' : { 'dark_mode' : newDarkMode } }
-    });
-  });
-};
-
-
+//= require_tree .
