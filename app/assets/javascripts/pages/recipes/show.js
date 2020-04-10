8a1b8b8e49587bdb4ba54d8b3181b7557adbac17
Jethro Williams <jethrowilliams4@hotmail.com>
2020-04-10 15:47:48 +0100

    Start js to identify clicks

--- app/assets/javascripts/pages/recipes/show.js
+++ app/assets/javascripts/pages/recipes/show.js
@@ -1,4 +1,22 @@
+const inputs = ['name', 'process']
+
 $(function() {
+  $(document).click(function(click) {
+    inputs.forEach((input) => {
+      if(inputIsEnabled(input) && clickIsOutsideInput(click, input)) {
+        // Save input
+        // After save, disable/hide input and show display
+      }
+    })
+  })
+
+  $('#name-display').click(function() {
+    $('#name-input').height($(this).height());
+    $(this).hide();
+    $('#name-input').removeClass('d-none');
+    $('#name-input').prop('disabled', false)
+  });
+
   $('#process-display').click(function() {
     $('#process-input').height($(this).height());
     $(this).hide();
@@ -7,4 +25,10 @@ $(function() {
   });
 });
 
-// .prop("disabled", true)
+function inputIsEnabled(input) {
+  return $(`#${input}-input`).prop('disabled') === false
+}
+
+function clickIsOutsideInput(click, input) {
+  return click.target.id !== `${input}-input`
+}
