99f6b2a9a478a8bf91ebe0902b61c1850afb9a54
Jethro Williams <jethrowilliams4@hotmail.com>
2020-04-11 09:49:03 +0100

    Click to enable/disable inputs

--- app/assets/javascripts/pages/recipes/show.js
+++ app/assets/javascripts/pages/recipes/show.js
@@ -5,23 +5,31 @@ $(function() {
     inputIdPrefixes.forEach((prefix) => {
       if(inputIsEnabled(prefix) && clickIsOutsideInput(click, prefix)) {
         // Save input
-        // After save, disable/hide input and show display
-        // Break loop
-      }
-    })
-  })
 
-  inputIdPrefixes.forEach((prefix) => {
-    $(`#${prefix}-display`).click(function() {
-      matchInputHeightToDisplayElement(prefix, this)
-      hideDisplayElement(this)
-      showInput(prefix)
+        // After save, disable/hide input and show display
+        hideInput(prefix)
+        showDisplayElement(prefix)
+
+        // Break loop
+        return false
+      }
+      if(click.target.id === `${prefix}-display`){
+        matchInputHeightToDisplayElement(prefix, click.target)
+        hideDisplayElement(click.target)
+        showInput(prefix)
+      }
     })
   })
 });
 
+const hideInput = (prefix) => $(`#${prefix}-input`).prop('disabled', true).addClass('d-none');
+function showDisplayElement(prefix){
+  $(`#${prefix}-display`).removeClass('d-none');
+}
+
 const matchInputHeightToDisplayElement = (prefix, displayElement) => $(`#${prefix}-input`).height($(displayElement).height());
-const hideDisplayElement = (displayElement) => $(displayElement).hide();
+const hideDisplayElement = (displayElement) => $(displayElement).addClass('d-none');
 const showInput = (prefix) => $(`#${prefix}-input`).removeClass('d-none').prop('disabled', false);
+
 const inputIsEnabled = (prefix) => $(`#${prefix}-input`).prop('disabled') === false
 const clickIsOutsideInput = (click, prefix) => click.target.id !== `${prefix}-input`
