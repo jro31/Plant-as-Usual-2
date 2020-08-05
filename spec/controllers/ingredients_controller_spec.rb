ce1a91537defcb386d76d4772c07e4ffa941e8d0
Jethro Williams <jethrowilliams4@hotmail.com>
2020-08-05 18:08:17 +0100

    Style wide recipe card

---
+++ spec/controllers/ingredients_controller_spec.rb
@@ -0,0 +1,18 @@
+# COMPLETE THIS
+
+RSpec.describe IngredientsController do
+  describe '#create' do
+    it do
+    end
+  end
+
+  describe '#update' do
+    it do
+    end
+  end
+
+  describe '#destroy' do
+    it do
+    end
+  end
+end
