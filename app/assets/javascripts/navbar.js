7e1f30814d8b6c370fb80a73630b3039032a76fd
Jethro Williams <jethrowilliams4@hotmail.com>
2020-08-12 17:12:35 +0100

    Add basic search box js

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,1 +1,9 @@
 // Try to get focus() on the search box when opened in the navbar
+// Would be good to disable all other page clicks when the search box is open
+$(function() {
+  $('#search-input').val("")
+})
+
+$('#search-input').change(function() {
+  $('#search-input').val().length > 0 ? $('#search-button').prop("disabled", false) : $('#search-button').prop("disabled", true);
+});
