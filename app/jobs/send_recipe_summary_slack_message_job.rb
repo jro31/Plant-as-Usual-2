ca920e25de040539ccad4f9acc2bf1d8e4d2780b
Jethro Williams <jethrowilliams4@hotmail.com>
2020-09-30 12:51:47 +0100

    Add comments

--- app/jobs/send_recipe_summary_slack_message_job.rb
+++ app/jobs/send_recipe_summary_slack_message_job.rb
@@ -1,3 +1,4 @@
+# Not currently called anywhere
 class SendRecipeSummarySlackMessageJob < ApplicationJob
   def perform
     SlackMessage.post_to_slack(Recipe.recipe_summary_message, nature: 'inform')
