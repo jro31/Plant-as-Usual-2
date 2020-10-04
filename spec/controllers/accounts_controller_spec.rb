6cf7f8e6fc6948f741c5e474990fed720d395d36
Jethro Williams <jethrowilliams4@hotmail.com>
2020-10-04 18:29:35 +0100

    Finish speccing accounts controller

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe AccountsController, type: :controller do
-  let(:user) { create(:user, username: 'aloy', email: 'aloy@ofthenora.tribe') }
+  let(:user) { create(:user, username: 'aloy', email: 'aloy@ofthenora.tribe', twitter_handle: 'allmotherwhisperer', instagram_handle: 'outkast_pix', website_url: 'https://www.aloysworld.com/') }
   before { sign_in user }
 
   describe 'GET #show' do
@@ -110,15 +110,117 @@ describe AccountsController, type: :controller do
     end
 
     describe 'twitter handle' do
-      # COMPLETE THIS
+      let(:params) { { user: { twitter_handle: new_twitter_handle } } }
+      context 'user is signed-in' do
+        context 'new twitter handle is valid' do
+          let(:new_twitter_handle) { 'watcher_hunter' }
+          it 'updates the twitter handle' do
+            expect(user.twitter_handle).to eq('allmotherwhisperer')
+            patch :update_user, params: params
+            expect(user.reload.twitter_handle).to eq('watcher_hunter')
+          end
+
+          it 'redirects to the account page' do
+            patch :update_user, params: params
+            expect(response).to redirect_to account_path
+          end
+        end
+
+        context 'new twitter handle is not valid' do
+          let(:new_twitter_handle) { '@fuck deathbringers' }
+          it 'does not update the twitter handle' do
+            expect(user.twitter_handle).to eq('allmotherwhisperer')
+            patch :update_user, params: params
+            expect(user.reload.twitter_handle).to eq('allmotherwhisperer')
+          end
+
+          it 'displays a flash message' do
+            patch :update_user, params: params
+            expect(flash[:alert]).to eq 'Fail. Twitter handle cannot contain spaces.'
+          end
+
+          it 'redirects to the account page' do
+            patch :update_user, params: params
+            expect(response).to redirect_to account_path
+          end
+        end
+      end
     end
 
     describe 'instagram handle' do
-      # COMPLETE THIS
+      let(:params) { { user: { instagram_handle: new_instagram_handle } } }
+      context 'user is signed-in' do
+        context 'new instagram handle is valid' do
+          let(:new_instagram_handle) { 'hades_who' }
+          it 'updates the instagram handle' do
+            expect(user.instagram_handle).to eq('outkast_pix')
+            patch :update_user, params: params
+            expect(user.reload.instagram_handle).to eq('hades_who')
+          end
+
+          it 'redirects to the account page' do
+            patch :update_user, params: params
+            expect(response).to redirect_to account_path
+          end
+        end
+
+        context 'new instagram handle is not valid' do
+          let(:new_instagram_handle) { 'faro is a dick' }
+          it 'does not update the instagram handle' do
+            expect(user.instagram_handle).to eq('outkast_pix')
+            patch :update_user, params: params
+            expect(user.reload.instagram_handle).to eq('outkast_pix')
+          end
+
+          it 'displays a flash message' do
+            patch :update_user, params: params
+            expect(flash[:alert]).to eq 'Fail. Instagram handle cannot contain spaces.'
+          end
+
+          it 'redirects to the account page' do
+            patch :update_user, params: params
+            expect(response).to redirect_to account_path
+          end
+        end
+      end
     end
 
     describe 'website url' do
-      # COMPLETE THIS
+      let(:params) { { user: { website_url: new_website_url } } }
+      context 'user is signed-in' do
+        context 'new website url is valid' do
+          let(:new_website_url) { 'http://www.proving-tips.com/' }
+          it 'updates the website url' do
+            expect(user.website_url).to eq('https://www.aloysworld.com/')
+            patch :update_user, params: params
+            expect(user.reload.website_url).to eq('http://www.proving-tips.com/')
+          end
+
+          it 'redirects to the account page' do
+            patch :update_user, params: params
+            expect(response).to redirect_to account_path
+          end
+        end
+
+        context 'new website url is not valid' do
+          let(:new_website_url) { 'www.seeker diaries.com' }
+          it 'does not update the website url' do
+            expect(user.website_url).to eq('https://www.aloysworld.com/')
+            patch :update_user, params: params
+            expect(user.reload.website_url).to eq('https://www.aloysworld.com/')
+          end
+
+          it 'displays a flash message' do
+            patch :update_user, params: params
+            expect(flash[:alert]).to eq 'Fail. Website url cannot contain spaces.'
+          end
+
+          it 'redirects to the account page' do
+            patch :update_user, params: params
+            expect(response).to redirect_to account_path
+          end
+        end
+      end
     end
   end
 end
