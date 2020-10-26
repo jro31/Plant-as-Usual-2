e0fb125794e46460b52277d939101e01d2b21fa3
Jethro Williams <jethrowilliams4@hotmail.com>
2020-10-26 19:05:20 +0000

    Working in development with SendGrid (instead of Gmail)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,11 +62,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
     domain:               'plantasusual.com',
-    user_name:            ENV['GMAIL_USER_NAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    user_name:            'apikey',
+    password:             ENV['SENDGRID_API_KEY'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
