2aaa322c2cfe3f831644c44ab4a2963116e844c1
Jethro Williams <jethrowilliams4@hotmail.com>
2020-10-27 12:27:34 +0000

    Update to use SendGrid as Heroku add-on

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,9 +65,9 @@ Rails.application.configure do
     address:              'smtp.sendgrid.net',
     port:                 587,
     domain:               'plantasusual.com',
-    user_name:            'apikey',
-    password:             ENV['SENDGRID_API_KEY'],
-    authentication:       'plain',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    authentication:       :plain,
     enable_starttls_auto: true
   }
 end
