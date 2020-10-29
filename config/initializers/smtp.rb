c9e89e920f38b8ab39e3eb051119f288ceb59a09
Jethro Williams <jethrowilliams4@hotmail.com>
2020-10-29 16:39:10 +0000

    Update to use Mailgun credentials

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  domain: 'plantasusual.com',
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  authentication: :plain,
-  enable_starttls_auto: true
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'plantasusual.com',
+  :authentication => :plain,
 }
