ff69d2431b74ac34fd1e8e9247a2386b17c18f71
Jethro Williams <jethrowilliams4@hotmail.com>
2020-11-09 17:32:59 +0000

    Move gmail settings to production only

--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'plantasusual.com',
-  user_name: ENV['GMAIL_USER_NAME'],
-  password: ENV['GMAIL_PASSWORD'],
-  authentication: :plain,
-  enable_starttls_auto: true
-}
